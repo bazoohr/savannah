d6dffa2d070bac596454f52d0336cc46291743cd
Francesco Allertsen <fallertsen@yahoo.it>
2012-10-30 23:15:32 +0100

    Get rid of fast_memcpy and fast_memset.
    Those functions are not needed for optimization. The compiler already does
    the optimization later.

--- kernel/include/string.h
+++ kernel/include/string.h
@@ -4,7 +4,7 @@
 #include <types.h>
 #include <cdef.h>
 
-void *  memcpy (void *dst, void *src, size_t count);
+void * memcpy (void *dst, const void *src, size_t count);
 void * memset (void *ptr, int c, size_t n);
 int memcmp(const void *cs, const void *ct, size_t count);
 
