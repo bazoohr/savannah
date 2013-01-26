53774536f7e8c63f15e561490d6233ff76f9beeb
Francesco Allertsen <fallertsen@yahoo.it>
2013-01-26 11:29:15 +0100

    Deleted and untracked all the automated files.

--- boot/stage2/include/asm_kernel_args_const.h
+++
@@ -1,9 +0,0 @@
-/* Auto generated file. Don't edit! */
-#ifndef __KERNEL_ARGS_CONST_H__
-#define __KERNEL_ARGS_CONST_H__
-
-#define KA_MEMSZ          0
-#define KA_KCR3          8
-#define KA_KEND_ADDR         16
-#define KA_INIT_ADDR         24
-#endif
